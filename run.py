d8a9d3c4960f0688726220c621b16ede9484eedd
Jon Chan <jchan@stackoverflow.com>
2014-09-04 12:34:35 -0400

    Fixed run port and host

--- run.py
+++ run.py
@@ -49,4 +49,5 @@ def PinBotMessage(message):
 
 if __name__ == "__main__":
 	app.debug = True
-	app.run(port = int(os.environ.get('PORT', 5000)))
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host = '0.0.0.0', port = port)
