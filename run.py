a3691ad2db72fcce5c88cc8dbb0e29bba3007111
Jon Chan <jchan@stackoverflow.com>
2014-09-04 12:27:04 -0400

    Added post and get handling

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ r = redis.from_url("redis://redistogo:273b30ab7575c845853f491ac4797121@grouper.r
 
 @app.route("/", methods = ['GET', 'POST'])
 def hello():
-	text = request.args["text"]
-	user_name = request.args["user_name"]
+	text = request.args.get("text", request.form["text"])
+	user_name = request.args.get("user_name", request.form["user_name"])
 	if (text == None):
 		return None
 	migrationNumber = 0
